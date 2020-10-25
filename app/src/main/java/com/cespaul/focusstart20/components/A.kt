c7f6855f7d2cb5862f547c7c7273f3569404496c
CesPaul <gumennikov.pavel@yandex.ru>
2020-10-25 11:03:52 +0700

    First commit. Implemented navigation.

---
+++ app/src/main/java/com/cespaul/focusstart20/components/A.kt
@@ -0,0 +1,31 @@
+package com.cespaul.focusstart20.components
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_a.*
+
+class A : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController: NavController = Navigation.findNavController(view)
+
+        to_b_button.setOnClickListener {
+            navController.navigate(R.id.action_a_to_b)
+        }
+    }
+}
---
+++ app/src/main/java/com/cespaul/focusstart20/components/B.kt
@@ -0,0 +1,29 @@
+package com.cespaul.focusstart20.components
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_b.*
+
+class B : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_b, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navController = Navigation.findNavController(view)
+
+        to_c_button.setOnClickListener {
+            navController.navigate(R.id.action_b_to_c)
+        }
+    }
+}
